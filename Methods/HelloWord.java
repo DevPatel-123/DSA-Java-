27fd570d0f5c24bd1217414086efb6345b604a60
DevPatel-123 <devvasani2005@gmail.com>
2026-01-22 09:14:29 +0000

    Methods

---
+++ Methods/HelloWord.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        greeting();
+    }
+    static void greeting() {
+        System.out.println("Hello World");
+    }
+}
